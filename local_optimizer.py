dbfc8c19150e1900fb3faed59be5ef728ee2bc24
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-12-08 22:04:22 -0500

    change to python function naming tradition

--- local_optimizer.py
+++ local_optimizer.py
@@ -1,6 +1,6 @@
 import re
 import string
-import tree.py
+import tree
 
 class compiler:
 	def __init__ (self, c_file):
@@ -18,7 +18,7 @@ class compiler:
 			wfile.write("\n")
 		wfile.close()
 
-	def Single_Assignment(self):
+	def single_assignment(self):
 		hashtable = {}
 		variable = list(string.ascii_lowercase)
 		variable_index = 25
@@ -40,36 +40,37 @@ class compiler:
 				variable_index = variable_index-1
 				self.code_array[index] = temp + self.code_array[index][1:]
 
-	def commonSubexpressionElimination(self):
+	def common_subexpression_elimination(self):
 		rhsList = []
 		for statement in self.code_array:
 			rhs = statement[statement.index('=') + 1:]
 			if rhs in rhsList:
-				self.replaceRhs(rhsList.index(rhs), self.code_array.index(statement), self.code_array[rhsList.index(rhs)].index("="), statement.index("="), self.code_array)
+				self.replace_rhs(rhsList.index(rhs), self.code_array.index(statement), self.code_array[rhsList.index(rhs)].index("="), statement.index("="), self.code_array)
 			rhsList.append(rhs)
 
 
-	def copyPropagation(self):
+	def copy_propagation(self):
 		for statement in self.code_array:
 			lhs = statement[:statement.index('=')]
 			rhs = statement[statement.index('=') + 1:]
 			if len(rhs) == 2:
 				temp = lhs[0]
-				self.code_array[self.code_array.index(statement) + 1:] = self.searchAndReplace(self.code_array[self.code_array.index(statement) + 1:], temp, rhs[1])
+				self.code_array[self.code_array.index(statement) + 1:] = self.search_and_replace(self.code_array[self.code_array.index(statement) + 1:], temp, rhs[1])
 
-	def searchAndReplace(self, myList, old, new):
+	def search_and_replace(self, myList, old, new):
 		for statement in myList:
 			myList[myList.index(statement)] = statement.replace(old, new)
 		return myList
 
-	def replaceRhs(self, indexList1, indexList2, indexString1, indexString2, myList):
+	def replace_rhs(self, indexList1, indexList2, indexString1, indexString2, myList):
 		myList[indexList2] = myList[indexList2][:indexString2 + 2] + myList[indexString1][:indexString1 - 1]
 
 def main():
 	c = compiler("test.txt")
-	c.Single_Assignment()
-	c.commonSubexpressionElimination()
-	c.copyPropagation()
+	c.single_assignment()
+	c.common_subexpression_elimination()
+	c.copy_propagation()
+	print c.code_array
 
 if __name__ == "__main__":
     main()
