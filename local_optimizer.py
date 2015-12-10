d8bc51d3fd7ef5648318ce036c4f55ac8ef112a6
Shane Skikne <shaneskikne@gmail.com>
2015-12-10 11:24:17 -0500

    added some array filtering

--- local_optimizer.py
+++ local_optimizer.py
@@ -13,7 +13,8 @@ class compiler:
 		rfile = open(self.file_name, "r")
 		self.code_orig = rfile.read()
 		rfile.close()
-		self.code_array = self.code_orig.split("\n")
+		self.code_array = self.filter(self.code_orig.split("\n"))
+
 		print self.code_array
 		self.constant_fold()
 		print self.code_array
@@ -23,6 +24,16 @@ class compiler:
 
 		self.optimize()
 		self.write()
+
+	def filter(self, array):
+		offset = 0
+		print array
+		for i in range(len(array)):
+			if (len(array[i+offset]) is 0) or array[i+offset].isspace():
+				del array[i+offset]
+				offset-= 1
+		print array
+		return array
 
 	def optimize(self):
 		preList = self.code_array[:]
