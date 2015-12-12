09bef42a01acacd2d945d847bdf504953c7c7194
Shane Skikne <shaneskikne@gmail.com>
2015-12-12 15:40:55 -0500

    added time and time averaging

--- wrapper.c
+++ wrapper.c
@@ -1,10 +1,26 @@
+#include <time.h>
 void wrapper_function() {
 	int a, b, c, d, e, f, g, h, i, j, k, l, 
 		m, n, o, p, q, r, s, t, u, v, w, x, y, z;
 
 }
-
+#define testCount 10
 int main () {
-	wrapper_function();
+	double times
+	for (int i = 0; i < testCount; i++){
+
+		clock_t begin, end;
+	  	double time_spent;
+	  	begin = clock();
+
+		wrapper_function();
+		end = clock();
+	  	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  		printf("Time spent: %f", time_spent );
+  		times = times + time_spent;
+  	}
+  	printf("Time spent: %f", times/testCount );
+  		
+
 	return 0;
 }
